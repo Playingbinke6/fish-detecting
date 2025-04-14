6c83f57f37107b464c5d0b8bacc8f1a9e7a6a51b
Teddy/Ember Clark <91207302+Playingbinke6@users.noreply.github.com>
2025-04-14 00:19:17 -0400

    updating some comments

--- HAAR-model/src/frame_finder.py
+++ HAAR-model/src/frame_finder.py
@@ -1,20 +1,24 @@
 import cv2
 import os
 
+"""
+Scan every nth frame of a video.
+Used this for collecting more seabed images for the model
+"""
 video_path = r'/HAAR-model/data\raw\Labeled_Fishes_In_The_Wild\_LABELED-FISHES-IN-THE-WILD\Test\Test_ROV_video_h264_decim.mp4'
 output_folder = r'C:\Users\playi\PycharmProjects\fish-detecting\input_data\non_fish_from_frame_script'
 os.makedirs(output_folder, exist_ok=True)
 
 cap = cv2.VideoCapture(video_path)
 frame_count = 0
+nth = 1
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # Manually inspect frames or use intervals
-    if frame_count % 1 == 0:  # every 50th frame
+    if frame_count % nth == 0:  # look at every nth frame
         filename = os.path.join(output_folder, f"neg_frame_{frame_count}.jpg")
         cv2.imwrite(filename, frame)
 
